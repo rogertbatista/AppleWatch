8b20ccd67a57baf7dc69e6cf96b9e21183b250f3
Roger Teixeira Batista <127768727+rogertbatista@users.noreply.github.com>
2024-02-01 11:41:55 -0300

    Refatoração

--- script.js
+++ script.js
@@ -1,59 +1,44 @@
 const imagemVisualizacao = document.getElementById('imagem-visualizacao');
 const tituloProduto = document.getElementById('titulo-produto');
 const nomeCor = document.getElementById('nome-cor-selecionada');
+const imagemMiniatura = [document.getElementById('0-imagem-miniatura'), document.getElementById('1-imagem-miniatura'), document.getElementById('2-imagem-miniatura')]
 
-const imagemMiniatura0 = document.getElementById('0-imagem-miniatura');
-const imagemMiniatura1 = document.getElementById('1-imagem-miniatura');
-const imagemMiniatura2 = document.getElementById('2-imagem-miniatura');
-
-const verdeCipreste = {
-    nome: 'Verde-cipreste',
-    pasta: 'imagens-verde-cipreste'
-}
-
-const azulInverno = {
-    nome: 'Azul-inverno',
-    pasta: 'imagens-azul-inverno'
-}
-
-const estelar = {
-    nome: 'Estelar',
-    pasta: 'imagens-estelar'    
-}
-
-const meiaNoite = {
-    nome: 'Meia-noite',
-    pasta: 'imagens-meia-noite'
-}
-
-const rosaClaro = {
-    nome: 'Rosa-claro',
-    pasta: 'imagens-rosa-claro'
-}
-
-const opcoesCores = [verdeCipreste, azulInverno, meiaNoite,estelar, rosaClaro];
+const opcoesCores = [
+    { nome: 'Verde-cipreste', pasta: 'imagens-verde-cipreste' },
+    { nome: 'Azul-inverno', pasta: 'imagens-azul-inverno' },
+    { nome: 'Meia-noite', pasta: 'imagens-meia-noite' },
+    { nome: 'Estelar', pasta: 'imagens-estelar' },
+    { nome: 'Rosa-claro', pasta: 'imagens-rosa-claro' }
+]
 const opcoesTamanho = ['41mm', '45mm'];
 
 let imagemSelecionada = 1;
 let tamanhoSelecionado = 1;
 let corSelecionada = 1;
 
-function trocarImagem(){
-    const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id;
-    imagemSelecionada = idOpcaoSelecionada.charAt(0);
+// Função para evitar repetição de código
+function trocaCorImagemMaior() {
     imagemVisualizacao.src = `./imagens/opcoes-cores/${opcoesCores[corSelecionada].pasta}/imagem-${imagemSelecionada}.jpeg`;
 }
 
-function trocarTamanho(){
-    // garantir que a variável de controle seja atualizada
+// Função para evitar repetição de código
+function ModificaTitulo() {
+    tituloProduto.innerText = `Pulseira loop esportiva ${opcoesCores[corSelecionada].nome} para caixa de ${opcoesTamanho[tamanhoSelecionado]}`;
+}
+
+function trocarImagem() {
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id;
+    imagemSelecionada = idOpcaoSelecionada.charAt(0);
+    trocaCorImagemMaior();
+}
+
+function trocarTamanho() {
     const idOpcaoSelecionada = document.querySelector('[name="opcao-tamanho"]:checked').id;
     tamanhoSelecionado = idOpcaoSelecionada.charAt(0);
-    // mudar o texto de acordo com o tamanho selecionado
-    tituloProduto.innerText = `Pulseira loop esportiva ${opcoesCores[corSelecionada].nome} para caixa de ${opcoesTamanho[tamanhoSelecionado]}`;
-    // trocar o tamanho da imagem
-    if(opcoesTamanho[tamanhoSelecionado] === '41mm'){
+    ModificaTitulo();
+    if (opcoesTamanho[tamanhoSelecionado] === '41mm') {
         imagemVisualizacao.classList.add('caixa-pequena');
-    }else{
+    } else {
         imagemVisualizacao.classList.remove('caixa-pequena');
     }
 }
@@ -61,13 +46,10 @@ function trocarTamanho(){
 function trocarCor() {
     const idOpcaoSelecionada = document.querySelector('[name="opcao-cor"]:checked').id;
     corSelecionada = idOpcaoSelecionada.charAt(0);
-
+    ModificaTitulo();
     nomeCor.innerText = `Cor - ${opcoesCores[corSelecionada].nome}`;
-    tituloProduto.innerText = `Pulseira loop esportiva ${opcoesCores[corSelecionada].nome} para caixa de ${opcoesTamanho[tamanhoSelecionado]}`
-
-    imagemMiniatura0.src = `./imagens/opcoes-cores/${opcoesCores[corSelecionada].pasta}/imagem-0.jpeg`;
-    imagemMiniatura1.src = `./imagens/opcoes-cores/${opcoesCores[corSelecionada].pasta}/imagem-1.jpeg`;
-    imagemMiniatura2.src = `./imagens/opcoes-cores/${opcoesCores[corSelecionada].pasta}/imagem-2.jpeg`;
-
-    imagemVisualizacao.src = `./imagens/opcoes-cores/${opcoesCores[corSelecionada].pasta}/imagem-${imagemSelecionada}.jpeg`;
+    trocaCorImagemMaior();
+    for (let i = 0; i <= 2; i++) {
+        imagemMiniatura[i].src = `./imagens/opcoes-cores/${opcoesCores[corSelecionada].pasta}/imagem-${i}.jpeg`;
+    }
 }
